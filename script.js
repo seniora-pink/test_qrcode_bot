13c46a4d21e74e12ba25eef9d77699ed730685ae
Bibisara <bibisara.o@prometeochain.io>
2023-08-16 15:14:31 +0600

    scan qr code

--- script.js
+++ script.js
@@ -62,13 +62,13 @@ const DemoApp = {
 
         // Make sure to replace 'YOUR_BOT_TOKEN' with your actual bot token
         const botToken = '6504257620:AAH4_RxmRPAWuIlQlpwwFirf6IziGssdKGQ';
-        const chatId = DemoApp.initDataUnsafe.chat.id;
-
+//        const chatId = DemoApp.initDataUnsafe.chat.id;
+        const chatId = 926320594
         const newParagraph = document.createElement("p");
         newParagraph.textContent = "CHAT ID: " + chatId;
         const sectionContainer = document.getElementById("section");
         sectionContainer.appendChild(newParagraph);
-//        Telegram.WebApp.showScanQrPopup({text: 'with any link'}, function (qrCode) {
+        Telegram.WebApp.showScanQrPopup({text: 'with any link'}, function (qrCode) {
             // Process the scanned QR code for order
             // For this example, we assume the QR code contains the order ID
             console.log("Scanned QR Code: qrCode");
@@ -92,14 +92,15 @@ const DemoApp = {
             .then(response => response.json())
             .then(data => {
                 console.log("Message sent:", data);
-//                DemoApp.close();
+                DemoApp.close();
             })
             .catch(error => {
                 console.error("Error sending message:", error);
-//                DemoApp.close();
+                DemoApp.close();
             });
 
-//            return true;
+            return true;
+        }
     }
 }
 
